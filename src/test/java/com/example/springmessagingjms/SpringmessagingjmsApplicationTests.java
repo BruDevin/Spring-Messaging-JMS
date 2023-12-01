8e4a3b1897bc40378e7393accc1a2da163f89b24
BruDevin <bruno.ads.2022@gmail.com>
2023-12-01 16:10:39 -0300

    Spring Messaging with JMS example

---
+++ src/test/java/com/example/springmessagingjms/SpringmessagingjmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmessagingjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmessagingjmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
